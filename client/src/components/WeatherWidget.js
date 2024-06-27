d72b9543ae1e8f526da549689f02a659eb079202
johnwaugh1 <waughjm@appstate.edu>
2024-06-27 15:41:04 -0400

    removed search feature from weather widget

--- client/src/components/WeatherWidget.js
+++ client/src/components/WeatherWidget.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import styles from '../styles.module.css'; // Import your CSS module
 
 const WeatherWidget = () => {
+  const [location, setLocation] = useState(null);
+
+  const fetchWeatherByLocation = useCallback((latitude, longitude) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a8d3de4af4087655a0d47aa2bd5699d8&units=imperial`)
+      .then(response => response.json())
+      .then(data => {
+        setLocation({ city: data.name });
+        displayWeather(data);
+      })
+      .catch(error => {
+        console.error('Error fetching weather data:', error);
+      });
+  }, []);
+
+  const fetchLocation = useCallback(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      fetchWeatherByLocation(latitude, longitude);
+    });
+  }, [fetchWeatherByLocation]);
+
+  useEffect(() => {
+    fetchLocation();
+
+    const intervalId = setInterval(fetchLocation, 300000); // Fetch location every 5 minutes
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [fetchLocation]);
+
   return (
     <section className={styles.weatherWidget}>
-      <div className={styles.weatherInput}>
-        <input type="text" id="cityInput" placeholder="Enter city" />
-        <button onClick={searchWeather}>Search</button>
-      </div>
       <div className={styles.weatherInfo} id="weatherInfo"></div>
     </section>
   );
-}
-
-function searchWeather() {
-  const city = document.getElementById('cityInput').value;
-
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8d3de4af4087655a0d47aa2bd5699d8&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-      displayWeather(data);
-    })
-    .catch(error => {
-      console.error('Error fetching weather data:', error);
-    });
 }
 
 function getWindDirection(angle) {
@@ -36,41 +49,6 @@ function capitalizeWords(str) {
     return char.toUpperCase();
   });
 }
-
-function searchWeatherByLocation() {
-  // Fetch the user's IP address
-  fetch('https://api.ipify.org?format=json')
-    .then(response => response.json())
-    .then(ipData => {
-      // Use the IP address to fetch geolocation data
-      fetch(`https://ipinfo.io/${ipData.ip}?token=607e2af9981046`)
-        .then(response => response.json())
-        .then(locationData => {
-          const city = locationData.city;
-          fetchWeatherByCity(city);
-        })
-        .catch(error => {
-          console.error('Error fetching geolocation data:', error);
-        });
-    })
-    .catch(error => {
-      console.error('Error fetching IP address:', error);
-    });
-}
-
-function fetchWeatherByCity(city) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8d3de4af4087655a0d47aa2bd5699d8&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-      displayWeather(data);
-    })
-    .catch(error => {
-      console.error('Error fetching weather data:', error);
-    });
-}
-
-// Call this function to fetch weather based on user's location
-searchWeatherByLocation();
 
 function displayWeather(data) {
   const weatherInfo = document.getElementById('weatherInfo');
@@ -87,14 +65,14 @@ function displayWeather(data) {
   const clouds = data.clouds.all;
 
   const weatherHTML = `
-      <img src="${iconUrl}" alt="Weather Icon" style="width: 200px; height: 200px;">
-      <h2>${cityName}</h2>
-      <h3>${temperature}°F</h3>
-      <p style="  margin-bottom: 50px; color: #333;">${description}</p>
-      <p>Clouds: ${clouds}% </p>
-      <p>Humidity: ${humidity}%</p>
-      <p>Wind Speed: ${windSpeed} MPH</p>
-      <p>Wind Direction: ${windDirection}</p>
+    <img src="${iconUrl}" alt="Weather Icon" style="width: 200px; height: 200px;">
+    <h2>${cityName}</h2>
+    <h3>${temperature}°F</h3>
+    <p style="  margin-bottom: 50px; color: #333;">${description}</p>
+    <p>Clouds: ${clouds}% </p>
+    <p>Humidity: ${humidity}%</p>
+    <p>Wind Speed: ${windSpeed} MPH</p>
+    <p>Wind Direction: ${windDirection}</p>
   `;
 
   weatherInfo.innerHTML = weatherHTML;
